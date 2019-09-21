ffce34716174ae062a621663ad1499a10d29cfbd
dasinlsb <399359237@qq.com>
2019-09-22 01:18:37 +0800

    add code for two projects

---
+++ 03-space-and-motion/SolarSystem/Assets/Scripts/SolarSystem.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SolarSystem : MonoBehaviour
+{
+	public const int starNum = 9;
+	public List<GameObject> stars;
+	private int sunId = 8;
+	private float disSunEarth = 149597870; // km
+	private float disOffset = 1f / 300000000;
+	private float sizeOffset = 1f / 100000;
+	private float rateOffset = 1f * 300;
+	
+	private string[] names = {
+		"水星",
+		"火星",
+		"天王星",
+		"金星",
+		"土星",
+		"地球",
+		"海王星",
+		"木星",
+		"太阳"
+	};
+	private double[] selfRadius = { // km
+		2440,
+		3397,
+		15559, //25559,
+		6052,
+		30268, //60268,
+		6378,
+		24764,
+		35492, //71492,
+		80000 // 69600
+	};
+	private double[] rotRadius = { // disSunEarth km
+		0.3871,
+		1.5237,
+		12, //19.2184,
+		0.7233,
+		9.5549,
+		1,
+		20, //30.1104,
+		5.2026,
+		0
+	};
+	private double[] selfPeriod = { // minute
+		59 * 24,
+		24,
+		17,
+		243 * 24,
+		10,
+		23,
+		16,
+		9,
+		32
+	};
+	private double[] rotPeriod = { // day
+		87.97,
+		687,
+		84.01 * 365,
+		225,
+		29.46 * 365,
+		365.26,
+		164.82 * 365,
+		11.86 * 365,
+		0
+	};
+
+    // Start is called before the first frame update
+    void renderCircles(float width) {
+    	var pts = new List<Vector3>();
+		LineRenderer line = gameObject.AddComponent<LineRenderer>();
+		for (int i = 0; i < starNum; i++) {
+			int pcnt = 100;
+			for (int j = 0; j < pcnt; j++) {
+				float angle = 2.0f * Mathf.PI * j / (float)pcnt;
+				float dis = (float)rotRadius[i] * disSunEarth * disOffset;
+				pts.Add(new Vector3(dis*Mathf.Cos(angle), 0, dis*Mathf.Sin(angle)));
+			}
+		}
+		line.SetWidth(width, width);
+		line.positionCount = pts.Count;
+		line.SetPositions(pts.ToArray()); 
+    }
+    void renderStars() {
+		for (int i = 0; i < starNum; i++) {
+        	stars.Add(GameObject.CreatePrimitive(PrimitiveType.Sphere));
+        	stars[i].transform.position = new Vector3((float)rotRadius[i] * disSunEarth * disOffset, 0, 0);
+        	float rad = (float)selfRadius[i] * sizeOffset;
+        	stars[i].transform.localScale = new Vector3 (rad, rad, rad);
+        }
+    }
+    void Start()
+    { 
+    	renderCircles(0.003f);
+		renderStars();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	for (int i = 0; i < stars.Count; i++) {
+    		if (i == sunId) {
+    			stars[i].transform.RotateAround(stars[sunId].transform.position, new Vector3(0, 1, 0), 0);
+    		} else {
+    			stars[i].transform.RotateAround(stars[sunId].transform.position, new Vector3(0, 1, 0),
+    								rateOffset * 365 * Time.deltaTime / (float)rotPeriod[i]);
+    		}
+    	}
+    }
+
+}
---
+++ 03-space-and-motion/priests-and-devils/Assets/Scripts/FirstController.cs
@@ -0,0 +1,207 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstController : MonoBehaviour, ISceneController, IUserAction
+{
+	private const int cntDevil = 3;
+	private const int cntPriest = 3;
+	private const int lenB = 8;
+	private Vector3 groundPosSrc = new Vector3(-4, 0, 0);
+	private Vector3 groundPosDst   = new Vector3(4, 0, 0);
+	private Vector3 groundPosLeft  = new Vector3(0, 0, 1);
+	private Vector3 groundPosRight = new Vector3(0, 0, -1);
+	private Vector3 boatPosSrc     = new Vector3(-2.5f, 1, 0);
+	private Vector3 boatPosDst     = new Vector3(2.5f, 1, 0);
+	private Vector3 rolePosSrc     = new Vector3(-4.5f, 1, 0);
+	private const float boatSpeed      = 4.0f;
+	private const float spacingOffset  = 0.2f;
+	// private Vector3 rolsPosDst     = new Vector3(4.5f, 1, 0);
+
+	private GameObject river, groundSrc, groundDst, groundLeft, groundRight, boat;
+	private List<GameObject> sideSrc, sideDst, passers;
+	private GameState gameState;
+	private MoveState boatState;
+	public enum GameState {
+		Playing, Win, Lose
+	}
+	public enum MoveState {
+		AtSrc, AtDst, FromSrc, FromDst
+	}
+    // Start is called before the first frame update
+    void Awake()
+    {
+    	SSDirector.getInstance().currentSceneController = this;
+    	LoadResources();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (boatState)
+        {
+        	case MoveState.FromSrc:
+        		boat.transform.position = Vector3.MoveTowards(boat.transform.position, boatPosDst, boatSpeed * Time.deltaTime);
+        		break;
+        	case MoveState.FromDst:
+        		boat.transform.position = Vector3.MoveTowards(boat.transform.position, boatPosSrc, boatSpeed * Time.deltaTime);
+        		break;
+        }
+        if (boat.transform.position == boatPosSrc) 
+        {
+        	boatState = MoveState.AtSrc;
+        }
+        if (boat.transform.position == boatPosDst)
+        {
+        	boatState = MoveState.AtDst;
+        }
+    }
+
+
+    /**
+     *  Utility
+     */
+
+    private List<GameObject> findRoles(List<GameObject> list, string name)
+    {
+    	return list.FindAll(role => role.name == name);
+    }
+
+    /**
+     *	ISceneController
+     */
+
+    public void LoadResources() {
+    	gameState = GameState.Playing;
+    	boatState = MoveState.AtSrc;
+    	river = Instantiate(Resources.Load<GameObject>("Prefabs/River"));
+    	groundSrc   = Instantiate(Resources.Load<GameObject>("Prefabs/Ground"), groundPosSrc,   Quaternion.identity);
+    	groundDst   = Instantiate(Resources.Load<GameObject>("Prefabs/Ground"), groundPosDst,   Quaternion.identity);
+    	groundLeft  = Instantiate(Resources.Load<GameObject>("Prefabs/Ground"), groundPosLeft,  Quaternion.identity);
+    	groundRight = Instantiate(Resources.Load<GameObject>("Prefabs/Ground"), groundPosRight, Quaternion.identity);
+    	boat = Instantiate(Resources.Load<GameObject>("Prefabs/Boat"), boatPosSrc, Quaternion.Euler(90, 90, 0));
+    	sideSrc = new List<GameObject>();
+    	sideDst = new List<GameObject>();
+    	passers = new List<GameObject>();
+    	for (var i = 0; i < cntDevil; i++) {
+    		Vector3 p = rolePosSrc - new Vector3(1+spacingOffset, 0, 0) * i;
+    		var devil = Instantiate(Resources.Load<GameObject>("Prefabs/Devil"), p, Quaternion.identity);
+    		devil.name = "Devil";
+    		sideSrc.Add(devil);
+    	}
+    	for (var i = 0; i < cntPriest; i++) {
+    		Vector3 p = rolePosSrc - new Vector3(1+spacingOffset, 0, 0) * (i+3);
+    		var priest = Instantiate(Resources.Load<GameObject>("Prefabs/Priest"), p, Quaternion.identity);
+    		priest.name = "Priest";
+    		sideSrc.Add(priest);
+    	}
+    }
+
+    /**
+   	 *  IUserActions
+   	 */
+
+   	public void Restart() 
+   	{
+   		Destroy(river);
+   		Destroy(groundSrc);
+   		Destroy(groundDst);
+   		Destroy(groundLeft);
+   		Destroy(groundRight);
+   		Destroy(boat);
+   		foreach (var i in sideSrc) {
+   			Destroy(i);
+   		}
+   		foreach (var i in sideDst) {
+   			Destroy(i);
+   		}
+   		foreach (var i in passers) {
+   			Destroy(i);
+   		}
+   		// Destroy(sideSrc);
+   		// Destroy(sideDst);
+   		// Destroy(passer);
+   		LoadResources();
+   	}
+
+   	public void BootBoat()
+   	{
+   		// if (gameState != GameState.Playing) return;
+   		switch (boatState) 
+   		{
+   			case MoveState.AtSrc:
+   				boatState = MoveState.FromSrc;
+   				break;
+   			case MoveState.AtDst:
+   				boatState = MoveState.FromDst;
+   				break;
+   			default:
+   				break;
+   		}
+   	}
+   	public void DownBoat(string name) 
+   	{
+   		// if (gameState != GameState.Playing) return;
+   		if (boatState != MoveState.AtDst) return;
+   		var list = findRoles(passers, name);
+   		if (list.Count == 0) return;
+   		var role = list[0];
+   		role.transform.parent = null;
+   		role.transform.localPosition = groundPosDst + 
+   						new Vector3(0, 1+spacingOffset) * (sideDst.Count + 1);
+   		sideDst.Add(role);
+   		passers.Remove(role);
+   		updateGameState();
+   	}
+   	public void UpBoat(string name) 
+   	{
+   		// if (gameState != GameState.Playing) return;
+   		if (boatState != MoveState.AtSrc) return;
+   		if (passers.Count >= 2) return;
+   		var list = findRoles(sideSrc, name);
+   		
+   		if (list.Count == 0) return;
+   		var role = list[0];
+   		role.transform.parent = boat.transform;
+   		role.transform.localPosition = boatPosSrc + new Vector3(2.5f, -1, 0) +
+   						new Vector3(0, 0, -1f-spacingOffset) * (passers.Count + 1);
+   		passers.Add(role);
+   		sideSrc.Remove(role);
+   		updateGameState();
+   	}
+   	public void DevilUpBoat()
+   	{
+   		UpBoat("Devil");
+   	}
+   	public void PriestUpBoat()
+   	{
+   		UpBoat("Priest");
+   	}
+   	public void DevilDownBoat()
+   	{
+   		DownBoat("Devil");
+   	}
+   	public void PriestDownBoat()
+   	{
+   		DownBoat("Priest");
+   	}
+   	private void updateGameState()
+   	{
+   		if (findRoles(sideSrc, "Priest").Count > 0 &&
+   			findRoles(sideSrc, "Devil").Count > findRoles(sideSrc, "Priest").Count) {
+   			gameState = GameState.Lose;
+   		}
+   		if (findRoles(sideDst, "Priest").Count > 0 &&
+   			findRoles(sideDst, "Devil").Count > findRoles(sideDst, "Priest").Count) {
+   			gameState = GameState.Lose;
+   		}
+   		if (sideDst.Count == cntPriest + cntDevil) {
+   			gameState = GameState.Win;
+   		}
+   	}
+   
+
+    public GameState GetGameState() {
+    	return gameState;
+    }
+}

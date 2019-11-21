c9fe56cdec7ec20353da088363a507e85c92c182
dasinlsb <399359237@qq.com>
2019-11-21 18:18:50 +0800

    add code for homework9

---
+++ 09-ui/health-bar/Assets/Scripts/Monster.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Monster : MonoBehaviour
+{
+	public Slider slider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	transform.Rotate(Vector3.up * 2.0f);
+    	if (Input.GetButtonDown("Fire1")) 
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                GameObject g = hit.transform.gameObject;
+                if (g == gameObject) 
+                {
+                	slider.value -= 0.05f;
+                	Debug.Log("hit myself");
+                }
+            }
+        }
+
+    }
+}
